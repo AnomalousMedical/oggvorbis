442f8ba238b9881debbb20d52c81f6539ef1c635
Andrew Piper <threax@users.noreply.github.com>
2014-12-24 17:00:09 +0000

    Grabbed current directory and used that to cd back to in auto build scripts.

--- AutoBuildOgg_x64.bat
+++ AutoBuildOgg_x64.bat
@@ -5,6 +5,7 @@ set SolutionName=libogg\win32\VS2010\libogg_static.sln
 set ThisFolder=%~dp0
 set RootDependencyFolder=%ThisFolder%..\
 set BuildCommand="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe"
+set CurrentDirectory=%CD%
 
 ::Actual build process
 %BuildCommand% "%SolutionName%" /property:Configuration=Debug;Platform=%Platform%
