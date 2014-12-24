7c4ab9fec8b29c8ac47e7653593bc62d42cfe2f9
AndrewPiper <AndrewPiper@localhost>
2014-12-24 16:46:08 +0000

    Added auto build for ogg and vorbis separatly.

---
+++ AutoBuildOgg_x64.bat
@@ -0,0 +1,11 @@
+set Platform=x64
+set SolutionName=libogg\win32\VS2010\libogg_static.sln
+
+::Less likely to need to change these.
+set ThisFolder=%~dp0
+set RootDependencyFolder=%ThisFolder%..\
+set BuildCommand="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe"
+
+::Actual build process
+%BuildCommand% "%SolutionName%" /property:Configuration=Debug;Platform=%Platform%
+%BuildCommand% "%SolutionName%" /property:Configuration=Release;Platform=%Platform%
