d8c4ed41f2e7e8b5595e82b50e1f6b81502bc7f0
AndrewPiper <AndrewPiper@localhost>
2015-03-18 15:06:28 +0000

    Modified all dependencies to build with armeabi-v7a.

--- AutoBuild_Android.bat
+++ AutoBuild_Android.bat
@@ -1,8 +1,10 @@
 pushd %~dp0
 cd libogg\android
-call ndk-build
+call ndk-build APP_ABI=armeabi-v7a clean
+call ndk-build APP_ABI=armeabi-v7a
 cd ../..
 cd libvorbis\android
-call ndk-build
+call ndk-build APP_ABI=armeabi-v7a clean
+call ndk-build APP_ABI=armeabi-v7a
 cd ../..
 popd
