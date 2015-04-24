a2cfe2c3a40ab998e8298c924321908ebad4e1ee
AndrewPiper <AndrewPiper@localhost>
2015-04-24 12:45:52 +0000

    Added x86 builds for android.

--- AutoBuild_Android_x86.bat
+++ AutoBuild_Android_x86.bat
@@ -2,11 +2,11 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\VsDevCmd
 
 pushd %~dp0
 cd libogg\android
-call ndk-build APP_ABI=armeabi-v7a clean
-call ndk-build APP_ABI=armeabi-v7a
+call ndk-build APP_ABI=x86 clean
+call ndk-build APP_ABI=x86
 cd ../..
 cd libvorbis\android
-call ndk-build APP_ABI=armeabi-v7a clean
-call ndk-build APP_ABI=armeabi-v7a
+call ndk-build APP_ABI=x86 clean
+call ndk-build APP_ABI=x86
 cd ../..
 popd
