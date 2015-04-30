29b6d2598714478cb387e6989807649845280795
Andrew Piper <threax@users.noreply.github.com>
2015-04-30 16:59:33 +0000

    Eliminated simulator builds for ios.

--- AutoBuild_iOS.sh
+++ AutoBuild_iOS.sh
@@ -11,28 +11,16 @@ THIS_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 #libogg
 BUILD_FOLDER=$THIS_FOLDER/libogg/ios
 cd $BUILD_FOLDER
-xcodebuild clean -sdk iphonesimulator8.1 SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
-xcodebuild clean -sdk iphoneos8.1 SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
+xcodebuild clean -sdk iphoneos8.3 SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
 
-xcodebuild -configuration Release -sdk iphonesimulator8.1 ONLY_ACTIVE_ARCH=NO ARCHS="i386 x86_64" SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
-xcodebuild -configuration Release -sdk iphoneos8.1 ONLY_ACTIVE_ARCH=NO ARCHS="armv7 armv7s arm64" SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
-
-rm -r build/Release
-mkdir build/Release
-lipo -create build/Release-iphoneos/libOgg.a build/Release-iphonesimulator/libOgg.a -output build/Release/libOgg.a
+xcodebuild -configuration Release -sdk iphoneos8.3 ONLY_ACTIVE_ARCH=NO ARCHS="armv7 armv7s arm64" SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
 
 #libvorbis
 BUILD_FOLDER=$THIS_FOLDER/libvorbis/ios
 cd $BUILD_FOLDER
-xcodebuild clean -sdk iphonesimulator8.1 SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
-xcodebuild clean -sdk iphoneos8.1 SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
+xcodebuild clean -sdk iphoneos8.3 SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
 
-xcodebuild -configuration Release -sdk iphonesimulator8.1 ONLY_ACTIVE_ARCH=NO ARCHS="i386 x86_64" SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
-xcodebuild -configuration Release -sdk iphoneos8.1 ONLY_ACTIVE_ARCH=NO ARCHS="armv7 armv7s arm64" SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
-
-rm -r build/Release
-mkdir build/Release
-lipo -create build/Release-iphoneos/libVorbis.a build/Release-iphonesimulator/libVorbis.a -output build/Release/libVorbis.a
+xcodebuild -configuration Release -sdk iphoneos8.3 ONLY_ACTIVE_ARCH=NO ARCHS="armv7 armv7s arm64" SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
 
 #Finish up
 cd "$START_PATH"
