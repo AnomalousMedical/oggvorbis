8201b21b6c895c8eb37b6af4dbda23d6c4a188b9
AndrewPiper <AndrewPiper@localhost>
2015-07-30 19:56:31 +0000

    Fixes to get dependencies to build under vs2015.

--- AutoBuildVorbis_x86.bat
+++ AutoBuildVorbis_x86.bat
@@ -4,9 +4,8 @@ set SolutionName=libvorbis\win32\VS2010\vorbis_static.sln
 ::Less likely to need to change these.
 set ThisFolder=%~dp0
 set RootDependencyFolder=%ThisFolder%..\
-set BuildCommand="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" /m
 set CurrentDirectory=%CD%
 
 ::Actual build process
-%BuildCommand% "%SolutionName%" /property:Configuration=Debug;Platform=%Platform%
-%BuildCommand% "%SolutionName%" /property:Configuration=Release;Platform=%Platform%
+msbuild.exe "%SolutionName%" /property:Configuration=Debug;Platform=%Platform%
+msbuild.exe "%SolutionName%" /property:Configuration=Release;Platform=%Platform%
