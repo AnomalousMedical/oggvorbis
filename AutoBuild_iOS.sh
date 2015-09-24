8d9fd6e245724a90ae7680cdeca039838f84ee0e
AndrewPiper <AndrewPiper@localhost>
2015-09-24 19:24:24 +0000

    Removed iOS sdks version numbers from build scripts.

--- AutoBuild_iOS.sh
+++ AutoBuild_iOS.sh
@@ -11,16 +11,16 @@ THIS_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 #libogg
 BUILD_FOLDER=$THIS_FOLDER/libogg/ios
 cd $BUILD_FOLDER
-xcodebuild clean -sdk iphoneos8.3 SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
+xcodebuild clean -sdk iphoneos SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
 
-xcodebuild -configuration Release -sdk iphoneos8.3 ONLY_ACTIVE_ARCH=NO ARCHS="armv7 armv7s arm64" SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
+xcodebuild -configuration Release -sdk iphoneos ONLY_ACTIVE_ARCH=NO ARCHS="armv7 armv7s arm64" SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
 
 #libvorbis
 BUILD_FOLDER=$THIS_FOLDER/libvorbis/ios
 cd $BUILD_FOLDER
-xcodebuild clean -sdk iphoneos8.3 SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
+xcodebuild clean -sdk iphoneos SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
 
-xcodebuild -configuration Release -sdk iphoneos8.3 ONLY_ACTIVE_ARCH=NO ARCHS="armv7 armv7s arm64" SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
+xcodebuild -configuration Release -sdk iphoneos ONLY_ACTIVE_ARCH=NO ARCHS="armv7 armv7s arm64" SHARED_PRECOMPS_DIR="$BUILD_FOLDER/SharedTmpPCH"
 
 #Finish up
 cd "$START_PATH"
